7c60da83d1c198a9b7d4cc17c07f025194adbdc5
Usman-Kashmiri <usmankashmiri378@gmail.com>
2023-05-17 20:19:46 +0500

    Added more fields on signup form & resolved map issues

--- src/components/Map.js
+++ src/components/Map.js
@@ -19,20 +19,45 @@ const Marker = ({ text, color }) => (
     {text}
   </div>
 );
-const Marker2 = () => <div style={{ color: "red", fontSize: "20px" }}>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" strokeWidth={0.2} stroke="black" width={35}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-  <path strokeLinecap="round" fill="white" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-</svg>
-</div>;
+const Marker2 = () => (
+  <div style={{ color: "red", fontSize: "20px" }}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="red"
+      viewBox="0 0 24 24"
+      strokeWidth={0.2}
+      stroke="black"
+      width={35}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+      />
+      <path
+        strokeLinecap="round"
+        fill="white"
+        strokeLinejoin="round"
+        d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+      />
+    </svg>
+  </div>
+);
 const Map = ({ hotels, center }) => {
-  console.log(center, 'center');
+  console.log(center, "center");
   return (
     <GoogleMapReact
       bootstrapURLKeys={{ key: "AIzaSyC7-xAxxhgl4VQLTURgqKdFlXrsiff4GUM" }}
-      center={center.lat !== "" ? center : { lat: 40.7127753, lng: -74.0059728 }}
-      defaultZoom={15}
+      center={
+        center.lat !== "" ? center : { lat: 40.7127753, lng: -74.0059728 }
+      }
+      defaultZoom={12}
       draggable={false}
+      options={{
+        zoomControl: false,
+        fullscreenControl: false,
+        keyboardShortcuts: false,
+      }}
     >
       {/* <Marker2
         lat={center?.lat}
