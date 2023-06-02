af8fc26170501d4659225739fd471de57a0e4891
Usman-Kashmiri <usmankashmiri378@gmail.com>
2023-06-02 18:06:03 +0500

    replaced bootstrap modal with antd modal

--- src/components/UrgentBookingModal.js
+++ src/components/UrgentBookingModal.js
@@ -1,54 +1,27 @@
+import { Modal } from "antd";
 import React, { useState } from "react";
 
 const UrgentBookingModal = ({ showModal, setShowModal }) => {
-  const [animation, setAnimation] = useState(showModal);
-
-  const handleClose = () => {
-    setAnimation(false);
-
-    setTimeout(() => {
-      setShowModal(false);
-    }, 600);
+  const handleOk = () => {
+    setShowModal(false);
   };
 
   return (
-    <div
-      className={`modal ${
-        animation === true ? "in" : "out"
-      } d-flex justify-content-center align-items-center`}
-    >
-      <div
-        className={`modal-dialog fade-scale ${
-          animation === true ? "in" : "out"
-        }`}
+    <>
+      <Modal
+        title="Basic Modal"
+        open={showModal}
+        onOk={handleOk}
+        cancelText={[]}
+        onCancel={handleOk}
+        centered
       >
-        <div className="modal-content">
-          <div className="modal-header">
-            <h3 className="modal-title">Urgent Booking</h3>
-            <button
-              type="button"
-              className="btn-close"
-              onClick={handleClose}
-            ></button>
-          </div>
-          <div className="modal-body">
-            <h5 className="text-capitalize">
-              For urgent same day bookings, please contact us directly at
-              ###-###-###
-            </h5>
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={handleClose}
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+        <h5 className="text-capitalize">
+          For urgent same day bookings, please contact us directly at
+          ###-###-###
+        </h5>
+      </Modal>
+    </>
   );
 };
 
